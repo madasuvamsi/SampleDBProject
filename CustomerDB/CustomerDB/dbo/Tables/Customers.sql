2f80f7ed1d6bc18dcfd94fd28106697493decd15
madasuvamsi <madasuvamsi@gmail.com>
2021-11-16 14:43:16 -0800

    Initial Commit

---
+++ CustomerDB/CustomerDB/dbo/Tables/Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [id]            INT            NOT NULL,
+    [CustomerName]  NVARCHAR (100) NULL,
+    [CustomerEmail] NVARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+

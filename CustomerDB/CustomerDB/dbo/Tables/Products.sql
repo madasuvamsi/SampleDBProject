a12b6d17d163f5eb8e7fbb772afa3ecd2bb8a69d
madasuvamsi <madasuvamsi@gmail.com>
2021-11-16 16:09:36 -0800

    Added ProductFamaily column

--- CustomerDB/CustomerDB/dbo/Tables/Products.sql
+++ CustomerDB/CustomerDB/dbo/Tables/Products.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Products] (
-    [Productid]   INT            NOT NULL,
-    [ProductName] NVARCHAR (100) NULL,
+    [Productid]     INT            NOT NULL,
+    [ProductName]   NVARCHAR (100) NULL,
+    [ProductFamily] NVARCHAR (150) NULL,
     PRIMARY KEY CLUSTERED ([Productid] ASC)
 );
+
+
 

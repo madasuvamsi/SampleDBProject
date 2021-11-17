a7cf597e8e966da1d5e7ea20d0d219c2193377b4
madasuvamsi <madasuvamsi@gmail.com>
2021-11-16 16:06:54 -0800

    Updated Customer schema

--- CustomerDB/CustomerDB/dbo/Tables/Customers.sql
+++ CustomerDB/CustomerDB/dbo/Tables/Customers.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[Customers] (
     [id]            INT            NOT NULL,
     [CustomerName]  NVARCHAR (100) NULL,
-    [CustomerEmail] NVARCHAR (255) NULL,
+    [CustomerEmail] NVARCHAR (300) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
 
 
